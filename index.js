9b625f9682ec9183f0c408fd40cbe443297e4ea3
Vadim Ivanov <v.ivanov@mobidev.biz>
2015-07-31 17:53:15 +0300

    navigation snake

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var game = {},
     canvas,
     ctx,
+    frame,
     keystate;
 
 game.data = {
@@ -21,7 +22,6 @@ game.data = {
 
 };
 var field = {
-
     init: function (dir, width, height) {
         for (var x = 0; x < width; x++) {
             game.data.map.push([]);
@@ -49,6 +49,7 @@ var snake = {
 
     move: function(x, y) {
         this.snakeMap.unshift({x: x, y: y});
+        console.log('snake move',this.snakeMap);
         this.lastPosition = this.snakeMap[0];
     },
 
@@ -66,7 +67,6 @@ function makeFood() {
         }
     }
     var foodPosition = foodMap[Math.round(Math.random()*(foodMap.length - 1))];
-    console.log(foodMap,foodPosition,Math.round(Math.random()*(foodMap.length - 1)));
     field.set(game.data.food, foodPosition.x, foodPosition.y);
 }
 
@@ -85,26 +85,69 @@ function start() {
     document.addEventListener("keyup", function(e) {
         delete keystate[e.keyCode];
     });
-
-    init();
+    initGame();
     main();
 }
 
-function init() {
-    var sp = {x: Math.floor(game.data.width/2), y: game.data.height - 1};
+function initGame() {
+    var snakePosition = {x: Math.floor(game.data.width/2), y: game.data.height - 1};
     field.init(game.data.empty, game.data.width, game.data.height);
-    field.set(game.data.snake, sp.x, sp.y);
-    snake.init(0, sp.x, sp.y);
+    field.set(game.data.snake, snakePosition.x, snakePosition.y);
+    snake.init(game.data.up, snakePosition.x, snakePosition.y);
     makeFood();
 }
 
 function main() {
-//    update();
+    update();
     paint();
     window.requestAnimationFrame(main, canvas);
 }
 
 function update() {
+    if (keystate[game.data.keyUp] && snake.direction != game.data.down) {
+        snake.direction = game.data.up;
+    }
+    if (keystate[game.data.keyDown] && snake.direction != game.data.up) {
+        snake.direction = game.data.down;
+    }
+    if (keystate[game.data.keyLeft] && snake.direction != game.data.left) {
+        snake.direction = game.data.left;
+    }
+    if (keystate[game.data.keyRight] && snake.direction != game.data.right) {
+        snake.direction = game.data.right;
+    }
+
+    var moveX = snake.lastPosition.x,
+        moveY = snake.lastPosition.y;
+        console.log(moveX, moveY);
+    switch (snake.direction) {
+        case game.data.up:
+            moveY -= 1;
+            break;
+        case game.data.down:
+            moveY += 1;
+            break;
+        case game.data.left:
+            moveX -= 1;
+            break;
+        case game.data.right:
+            moveX += 1;
+            break;
+    }
+    if (0 >= moveY || moveY > game.data.height - 1 ||
+        0 >= moveX || moveX > game.data.width - 1 ||
+        game.data.map[moveX][moveY] == game.data.snake) {
+//        initGame();
+    }
+
+    if (game.data.map[moveX][moveY] == game.data.food) {
+        makeFood();
+    } else {
+        var cell = snake.remove();
+        field.set(game.data.empty, cell.x, cell.y);
+    }
+        field.set(game.data.snake, moveX, moveY);
+        snake.move(moveX, moveY);
 
 }
 
