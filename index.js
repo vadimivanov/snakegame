fae434616628b9eea0c3983ea6977522fc217216
Vadim <mr.vadim.ivanov@gmail.com>
2015-08-02 17:36:05 +0300

    optimization animation and add counter for food

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var game = {},
     canvas,
     ctx,
-    frame,
+    animationFrame,
     keystate;
 
 game.data = {
-    map: [],
+    count: 1,
     width: 20,
     height: 20,
     empty: 0,
@@ -22,52 +22,60 @@ game.data = {
 
 };
 var field = {
+    width: null,
+    height: null,
+    fieldMap: [],
+
     init: function (dir, width, height) {
+        this.width = width;
+        this.height = height;
+        this.fieldMap = [];
+
         for (var x = 0; x < width; x++) {
-            game.data.map.push([]);
+            this.fieldMap.push([]);
             for (var y = 0; y < height; y++) {
-                game.data.map[x].push(dir);
+                this.fieldMap[x].push(dir);
             }
         }
     },
-    set: function(val, x, y) {
-        game.data.map[x][y] = val;
+    set: function (val, x, y) {
+        this.fieldMap[x][y] = val;
     },
 
-    get: function(x, y) {
-        return  game.data.map[x][y];
+    get: function (x, y) {
+        return  this.fieldMap[x][y];
     }
 };
 var snake = {
-    snakeMap: [],
+    snakeMap: null,
     direction: null,
     lastPosition: null,
-    init: function(dir, x, y) {
+
+    init: function (dir, x, y) {
         this.direction = dir;
+        this.snakeMap = [];
         this.move(x, y);
     },
 
-    move: function(x, y) {
+    move: function (x, y) {
         this.snakeMap.unshift({x: x, y: y});
-        console.log('snake move',this.snakeMap);
         this.lastPosition = this.snakeMap[0];
     },
 
-    remove: function() {
+    remove: function () {
         return this.snakeMap.pop();
     }
 };
 function makeFood() {
-    var foodMap = [];
-    for (var x = 0; x < game.data.width; x++) {
-        for (var y = 0; y < game.data.height; y++) {
-            if (field.get(x, y) === game.data.empty) {
-                foodMap.push({x: x, y: y});
-            }
+    var check = 0;
+    while (check < game.data.count) {
+        var foodPositionX = Math.floor(field.width * Math.random()),
+            foodPositionY = Math.floor(field.height * Math.random());
+        if (field.get(foodPositionX, foodPositionY) == game.data.empty) {
+            field.set(game.data.food, foodPositionX, foodPositionY);
+            check++;
         }
     }
-    var foodPosition = foodMap[Math.round(Math.random()*(foodMap.length - 1))];
-    field.set(game.data.food, foodPosition.x, foodPosition.y);
 }
 
 function start() {
@@ -78,22 +86,24 @@ function start() {
     ctx = canvas.getContext("2d");
     container.appendChild(canvas);
 
+    animationFrame = 0;
     keystate = {};
-    document.addEventListener("keydown", function(e) {
+    document.addEventListener("keydown", function (e) {
         keystate[e.keyCode] = true;
     });
-    document.addEventListener("keyup", function(e) {
+    document.addEventListener("keyup", function (e) {
         delete keystate[e.keyCode];
     });
     initGame();
     main();
+
 }
 
 function initGame() {
-    var snakePosition = {x: Math.floor(game.data.width/2), y: game.data.height - 1};
     field.init(game.data.empty, game.data.width, game.data.height);
-    field.set(game.data.snake, snakePosition.x, snakePosition.y);
+    var snakePosition = {x: Math.floor(game.data.width/2), y: game.data.height - 1};
     snake.init(game.data.up, snakePosition.x, snakePosition.y);
+    field.set(game.data.snake, snakePosition.x, snakePosition.y);
     makeFood();
 }
 
@@ -104,6 +114,8 @@ function main() {
 }
 
 function update() {
+    animationFrame++;
+
     if (keystate[game.data.keyUp] && snake.direction != game.data.down) {
         snake.direction = game.data.up;
     }
@@ -117,38 +129,38 @@ function update() {
         snake.direction = game.data.right;
     }
 
-    var moveX = snake.lastPosition.x,
-        moveY = snake.lastPosition.y;
-        console.log(moveX, moveY);
-    switch (snake.direction) {
-        case game.data.up:
-            moveY -= 1;
-            break;
-        case game.data.down:
-            moveY += 1;
-            break;
-        case game.data.left:
-            moveX -= 1;
-            break;
-        case game.data.right:
-            moveX += 1;
-            break;
-    }
-    if (0 >= moveY || moveY > game.data.height - 1 ||
-        0 >= moveX || moveX > game.data.width - 1 ||
-        game.data.map[moveX][moveY] == game.data.snake) {
-//        initGame();
-    }
+    if (animationFrame%5 == 0) {
+        var moveX = snake.lastPosition.x,
+            moveY = snake.lastPosition.y;
+        switch (snake.direction) {
+            case game.data.up:
+                moveY -= 1;
+                break;
+            case game.data.down:
+                moveY += 1;
+                break;
+            case game.data.left:
+                moveX -= 1;
+                break;
+            case game.data.right:
+                moveX += 1;
+                break;
+        }
+        if (0 > moveY || moveY > field.height - 1 ||
+            0 > moveX || moveX > field.width - 1 ||
+            field.get(moveX, moveY) == game.data.snake) {
+           return initGame();
+        }
 
-    if (game.data.map[moveX][moveY] == game.data.food) {
-        makeFood();
-    } else {
-        var cell = snake.remove();
-        field.set(game.data.empty, cell.x, cell.y);
-    }
+        if (field.get(moveX, moveY) == game.data.food) {
+            makeFood();
+        } else {
+            var cell = snake.remove();
+            field.set(game.data.empty, cell.x, cell.y);
+        }
         field.set(game.data.snake, moveX, moveY);
         snake.move(moveX, moveY);
-
+    }
 }
 
 function paint() {
@@ -167,7 +179,6 @@ function paint() {
                     ctx.fillStyle = "#ff0000";
                     break;
             }
-
             ctx.fillRect(x*cellW, y*cellH, cellW, cellH);
         }
     }
